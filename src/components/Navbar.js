23dd826ff9b7fb7ade112143ba9e8452a7a62a96
ANS <36783010+answebdev@users.noreply.github.com>
2022-09-19 20:20:29 -0700

    add badge to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { Flex, Icon, Image } from '@chakra-ui/react';
+import { Flex, Box, Icon, Image, Badge } from '@chakra-ui/react';
 import { MdMenu, MdShoppingBasket } from 'react-icons/md';
 import { ShopContext } from '../context/shopContext';
 import logo from '../img/Logologo_1.svg';
@@ -13,6 +13,7 @@ const Navbar = () => {
       backgroundColor='#ffa8e2'
       flexDir='row'
       justifyContent='space-between'
+      alignItems='center'
       p='2rem' // Padding
     >
       <Icon
@@ -26,14 +27,19 @@ const Navbar = () => {
       <Link to='/'>
         <Image src={logo} w={100} h={100} />
       </Link>
-      <Icon
-        onClick={() => openCart()}
-        fill='white'
-        cursor='pointer'
-        as={MdShoppingBasket}
-        w={30}
-        h={30}
-      ></Icon>
+      <Box>
+        <Icon
+          onClick={() => openCart()}
+          fill='white'
+          cursor='pointer'
+          as={MdShoppingBasket}
+          w={30}
+          h={30}
+        />
+        <Badge backgroundColor='#FF38BD' borderRadius='50%'>
+          {checkout?.lineItems?.length}
+        </Badge>
+      </Box>
     </Flex>
   );
 };
