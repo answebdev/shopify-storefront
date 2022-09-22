347bf88dffb59f8e1c764a88a5d0dcecd86b5c08
ANS <36783010+answebdev@users.noreply.github.com>
2022-09-21 19:56:39 -0700

    add image with text section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { ShopContext } from '../context/shopContext';
 import { Box, Grid, Text, Image } from '@chakra-ui/react';
 import Hero from '../components/Hero';
+import ImageWithText from '../components/ImageWithText';
 
 const Home = () => {
   const { fetchAllProducts, products } = useContext(ShopContext);
@@ -33,6 +34,19 @@ const Home = () => {
           </Link>
         ))}
       </Grid>
+      <ImageWithText
+        button
+        image='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758'
+        heading='First Heading'
+        text="I'm baby kale chips twee skateboard tattooed, DIY iPhone ugh mixtape tumeric unicorn narwhal. Iceland shoreditch authentic, sartorial vegan twee flannel banh mi bushwick retro farm-to-table single-origin coffee."
+      />
+      <ImageWithText
+        reverse
+        button
+        image='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/bath-bomb-and-candle.jpg?v=1610066758'
+        heading='Second Heading'
+        text="I'm baby kale chips twee skateboard tattooed, DIY iPhone ugh mixtape tumeric unicorn narwhal. Iceland shoreditch authentic, sartorial vegan twee flannel banh mi bushwick retro farm-to-table single-origin coffee."
+      />
     </Box>
   );
 };
